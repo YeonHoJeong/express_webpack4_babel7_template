5c2cf66b5676428a22be9bcb38463c39c69a46aa
YeonHoJeong <dusgh2101@naver.com>
2018-10-29 11:34:34 +0900

    현재 webpack 번들링 제외 , Babel-node로 실행하는 것 까지만

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+let router = express.Router();
+//import data from './data';
+
+
+//router.use('/data', data);
+
+module.exports = router;
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-console.log(iAmJavascriptES6);
